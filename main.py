6072273fcc0db94d9d5376bac0de6229646229b3
rahul <rahulkr18499@gmail.com>
2026-04-22 15:48:13 +0530

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+import json
+
 app = FastAPI()
+
+def load_data():
+    with open("patients.json", "r") as f:
+        data = json.load(f)
+    return data
+
 @app.get("/")
 def hello():
-    return {"message": "Hello World"}
+    return {"message": "Patient Management System API"}
 
 
 @app.get('/about')
 def about():
-    return {"message": "This is a simple FastAPI application."}
+    return {"message": "A fully functional API to manage your patients records"}
 
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data
+
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example = 'P001')):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    return {'error': 'Patient not found'}
